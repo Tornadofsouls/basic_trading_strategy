f440a3b0f4a7b6f8eaa4d1a11bd87d367de98e43
chendi <chenditc@umich.edu>
2022-07-31 09:49:31 +0000

    Add script to find best hyper params

---
+++ strategies/qlib_strategy/find_hyperprams.py
@@ -0,0 +1,86 @@
+
+import qlib
+import pandas as pd
+from qlib.constant import REG_CN
+from qlib.utils import exists_qlib_data, init_instance_by_config
+from qlib.workflow import R
+from qlib.workflow.record_temp import SignalRecord, PortAnaRecord
+from qlib.utils import flatten_dict
+import optuna
+from myalpha158 import MyAlpha158
+import fire
+
+def get_dataset():
+  data_handler_config = {
+      "start_time": "2001-01-01",
+      "end_time": "2018-01-01",
+      "fit_start_time": "2001-01-01",
+      "fit_end_time": "2014-01-01",
+      "instruments": "csi300",
+      "benchmark": "SH000300"
+  }    
+  dataset_config = {
+      "class": "DatasetH",
+      "module_path": "qlib.data.dataset",
+      "kwargs": {
+          "handler": MyAlpha158(**data_handler_config),
+          "segments": {
+              "train": (data_handler_config["fit_start_time"], data_handler_config["fit_end_time"]),
+              "valid": ("2014-01-01", "2018-01-01"),
+          },
+      },
+  }
+
+  return init_instance_by_config(dataset_config)
+
+dataset = None
+
+def objective(trial):
+    task = {
+        "model": {
+            "class": "LGBModel",
+            "module_path": "qlib.contrib.model.gbdt",
+            "kwargs": {
+                "loss": "mse",
+                "colsample_bytree": trial.suggest_uniform("colsample_bytree", 0.5, 1),
+                "learning_rate": trial.suggest_uniform("learning_rate", 0, 1),
+                "subsample": trial.suggest_uniform("subsample", 0, 1),
+                "lambda_l1": trial.suggest_loguniform("lambda_l1", 1e-8, 1e4),
+                "lambda_l2": trial.suggest_loguniform("lambda_l2", 1e-8, 1e4),
+                "max_depth": 10,
+                "num_leaves": trial.suggest_int("num_leaves", 1, 1024),
+                "feature_fraction": trial.suggest_uniform("feature_fraction", 0.4, 1.0),
+                "bagging_fraction": trial.suggest_uniform("bagging_fraction", 0.4, 1.0),
+                "bagging_freq": trial.suggest_int("bagging_freq", 1, 7),
+                "min_data_in_leaf": trial.suggest_int("min_data_in_leaf", 1, 50),
+                "min_child_samples": trial.suggest_int("min_child_samples", 5, 100),
+            },
+        },
+    }
+    evals_result = dict()
+    model = init_instance_by_config(task["model"])
+    model.fit(dataset, evals_result=evals_result)
+    return min(evals_result["valid"]["l2"])
+
+def optimize_model_hyperparam():
+  global dataset
+
+  # use default data
+  # NOTE: need to download data from remote: python scripts/get_data.py qlib_data_cn --target_dir ~/.qlib/qlib_data/cn_data
+  provider_uri = "~/.qlib/qlib_data/cn_data"  # target_dir
+  #provider_uri = "/mnt/investment_data/qlib_bin"
+  if not exists_qlib_data(provider_uri):
+      print(f"Qlib data is not found in {provider_uri}")
+      sys.path.append(str(scripts_dir))
+      from get_data import GetData
+      GetData().qlib_data(target_dir=provider_uri, region=REG_CN)
+  qlib.init(provider_uri=provider_uri, region=REG_CN)
+
+  dataset = get_dataset()
+  
+  optuna.create_study(study_name="LGBM_158_with_benchmark", direction="minimize", storage="sqlite:///optuna.db.sqlite3", load_if_exists=True)
+  study = optuna.Study(study_name="LGBM_158_with_benchmark", storage="sqlite:///optuna.db.sqlite3")
+  study.optimize(objective, n_trials=2000)
+
+if __name__ == "__main__":
+  fire.Fire(optimize_model_hyperparam)
---
+++ strategies/qlib_strategy/myalpha158.py
@@ -0,0 +1,45 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from qlib.contrib.data.handler import Alpha158, _DEFAULT_LEARN_PROCESSORS, DataHandlerLP
+
+class MyAlpha158(Alpha158):
+    def __init__(
+        self,
+        instruments="csi500",
+        start_time=None,
+        end_time=None,
+        freq="day",
+        infer_processors=[],
+        learn_processors=_DEFAULT_LEARN_PROCESSORS,
+        fit_start_time=None,
+        fit_end_time=None,
+        process_type=DataHandlerLP.PTYPE_A,
+        filter_pipe=None,
+        inst_processor=None,
+        benchmark = "SH000300",
+        **kwargs,
+    ):
+        self.benchmark = benchmark
+        super().__init__(instruments, start_time, end_time, freq, infer_processors, learn_processors, fit_start_time, fit_end_time, process_type, filter_pipe, inst_processor, **kwargs)
+        
+
+    def get_feature_config(self):
+        conf = {
+            "kbar": {},
+            "price": {
+                "windows": [0],
+                "feature": ["OPEN", "HIGH", "LOW", "CLOSE"],
+            },
+            "rolling": {},
+        }
+        normal_fields, normal_names = self.parse_config_to_fields(conf)
+        benchmark_fields = ["ChangeInstrument('{}', {})".format(self.benchmark, field) for field in normal_fields ]
+        benchmark_names = ["benchmark_{}".format(self.benchmark, name) for name in normal_names ]
+        
+        return normal_fields + benchmark_fields, normal_names + benchmark_names
+
+    def get_label_config(self):
+        return (["Ref($open, -4)/Ref($open, -1) - 1"], ["LABEL0"])
+
+
